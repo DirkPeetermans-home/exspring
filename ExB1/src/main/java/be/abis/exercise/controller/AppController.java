93432a5cd2e3da444250b6d312584354a780785b
Dirk-Peetermans-home <peetermans.dirk@telenet.be>
2020-10-09 14:21:00 +0200

    ExB1

--- ExB1/src/main/java/be/abis/exercise/controller/AppController.java
+++ ExB1/src/main/java/be/abis/exercise/controller/AppController.java
@@ -4,15 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import be.abis.exercise.model.Login;
+import be.abis.exercise.model.Person;
 import be.abis.exercise.service.TrainingService;
 
 @Controller
 public class AppController {
 	
+	
 	@Autowired
 	TrainingService trainingService;
+	
+	Person validLoginPerson;
 	
 	@GetMapping("/")
 	public String login(Model model){
@@ -20,6 +25,16 @@ public class AppController {
 		model.addAttribute("loginScreen",login);
 		
 		return "loginScreen"; 
+				
 	}
+	
+	@PostMapping("/")
+	public String welcome (Model model, Login login) {
+		
+		validLoginPerson = trainingService.findPerson(login.getEmail(), login.getPsw()) ;
+		if (validLoginPerson != null);
+		return "welcome";
+	}
+	
 
 }
