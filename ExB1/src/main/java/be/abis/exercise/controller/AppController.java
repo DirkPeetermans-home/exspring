4779da863a86f4f7894dea90cce7683dc1033771
Dirk-Peetermans-home <peetermans.dirk@telenet.be>
2020-10-12 13:49:02 +0200

    ExB2

--- ExB1/src/main/java/be/abis/exercise/controller/AppController.java
+++ ExB1/src/main/java/be/abis/exercise/controller/AppController.java
@@ -1,11 +1,15 @@
 package be.abis.exercise.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import be.abis.exercise.model.Course;
 import be.abis.exercise.model.Login;
 import be.abis.exercise.model.Person;
 import be.abis.exercise.service.TrainingService;
@@ -18,6 +22,7 @@ public class AppController {
 	TrainingService trainingService;
 	
 	Person validLoginPerson;
+	List<Course> allCourses;
 	
 	@GetMapping("/")
 	public String login(Model model){
@@ -47,10 +52,88 @@ public class AppController {
 				
 	}
 
-	@GetMapping("/dummy")
-	public String dummy(){
+	@GetMapping("/searchforcourses")
+	public String searchForCourses(){
 				
-		return "redirect:/dummyPage"; 
+		return "/searchforcourses"; 
 				
+	}
+	
+	@GetMapping("/personadministration")
+	public String personAdministrations(){
+				
+		return "/personadministration"; 
+				
+	}
+	
+	@GetMapping("/back")
+	public String back(Model model, Person validloginPerson){
+		model.addAttribute("attrib1",validLoginPerson);	
+		return "welcome"; 
+				
+	}
+	
+	@GetMapping("/showallcourses")
+		public String showAllCourses(Model model){
+		model.addAttribute("attrib1",allCourses);
+		return "/showallcourses"; 
+				
+	}
+	
+	@GetMapping("/findcoursebyall")
+	public String findcoursebyall(Model model){
+		allCourses = trainingService.getCourseService().findAllCourses();
+		model.addAttribute("attrib1",allCourses);
+		return "redirect:/showallcourses"; 
+	}
+	
+	@GetMapping("/findcoursebyid")
+	public String findcoursebyid(Model model){
+		Course c1=new Course();
+		model.addAttribute("attrib1",c1);
+		
+		return "findcoursebyid"; 
+				
+	} 
+	
+	@PostMapping("/findcoursebyid")
+	public String findCoursebyId (Course c1) {
+		
+		int id = Integer.parseInt(c1.getCourseId());
+		Course courseById = trainingService.getCourseService().findCourse(id);
+		
+		if (courseById != null) {
+			allCourses = new ArrayList<Course>();
+			allCourses.add(courseById);
+			return "redirect:/showallcourses";
+		} else {
+			return "redirect:/findcoursebyid";
+		}
+						
+	}
+	
+	@GetMapping("/findcoursebyshorttitle")
+	public String findcoursebyShortTitle(Model model){
+		Course c2=new Course();
+		model.addAttribute("attrib1",c2);
+		
+		return "findcoursebyshorttitle"; 
+				
+	} 
+	
+	@PostMapping("/findcoursebyshorttitle")
+	public String findCoursebyShortTitle (Course c2) {
+		
+		String shortTitle = c2.getShortTitle();
+		Course courseByShortTitle = trainingService.getCourseService().findCourse(shortTitle);
+		System.out.println(courseByShortTitle);
+		if (courseByShortTitle != null) {
+			allCourses = new ArrayList<Course>();
+			allCourses.add(courseByShortTitle);
+			return "redirect:/showallcourses";
+		} else {
+			return "redirect:/findcoursebyshorttitle";
+		}
+			
 	}
 }
