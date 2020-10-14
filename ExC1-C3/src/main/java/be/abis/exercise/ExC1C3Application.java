c33cad41a2de55dced59fbf769449dc9c76b7c4e
Dirk-Peetermans-home <peetermans.dirk@telenet.be>
2020-10-14 10:45:58 +0200

    ExC1

---
+++ ExC1-C3/src/main/java/be/abis/exercise/ExC1C3Application.java
@@ -0,0 +1,13 @@
+package be.abis.exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExC1C3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExC1C3Application.class, args);
+	}
+
+}
---
+++ ExC1-C3/src/test/java/be/abis/exercise/ExC1C3ApplicationTests.java
@@ -0,0 +1,13 @@
+package be.abis.exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExC1C3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
